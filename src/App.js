79578f411b0450a3d79522f1130c2f630c7af8d4
Matt <mddever@gmail.com>
2019-07-11 21:33:39 -0600

    added state and change date function

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import DateInput from "./components/DateInput";
 import Photo from "./components/Photo";
 
 class App extends Component {
+
+    state = {
+    date: "",
+    photo: "",
+  };
+
+  /// this is the change date function
+  changeDate = e => {
+    e.preventDefault();
+    console.log(e.target);
+  };
   render () {
     return (
       <div>
         <h1>NASA'S Astronomy Picture of the Day</h1>
-        <DateInput />
+        <DateInput 
+        changeDate={this.changeDate}/>
         <Photo />
       </div>
     )
