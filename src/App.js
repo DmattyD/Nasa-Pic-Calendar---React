131641eb4e5707de7f0da753e273b20a66b09591
Matt <mddever@gmail.com>
2019-07-16 17:26:08 -0600

    restarted due to multiple errors

--- src/App.js
+++ src/App.js
@@ -1,59 +1,15 @@
 import React, { Component } from 'react';
-import moment from 'moment';
-import DateInput from "./components/DateInput";
-import Photo from "./components/Photo";
+import DateInput from './components/DateInput';
+import Photo from './components/Photo.js'
 
 class App extends Component {
-
-    state = {
-    date:'',
-    photo: "",
-  };
-
-  
-  /// Not sure why moment isn't working
-  // formatDate = moment => {
-  //   let year = moment.year();
-  //   let month = moment.month() + 1;
-  //   let day = moment.day();
-  //   return `${year}-${month}-${day}`;
-  // }
-
-  /// sets state of the photo to the selected date inputed into the form
-  getPhoto = date => {
-    fetch(`
-    https://api.nasa.gov/planetary/apod?date=${date}&api_key=58oXjtN6gnphNMkCExZ8VSyl6effQwda2CppFiYV`)
-    .then(response => response.json())
-    .then(photoData => this.setState({ photo: photoData }));
-  };
-
-  ////this is the API fetch
-  componentDidMount() {
-    fetch (`
-    https://api.nasa.gov/planetary/apod?api_key=58oXjtN6gnphNMkCExZ8VSyl6effQwda2CppFiYV`)
-    .then(response => response.json())
-    .then(json => this.setState({ photo: json}));
-  }
-
-  /// this is the change date function
-  changeDate = e => {
-    e.preventDefault();
-    let dateFromInput = e.target[0].value
-    this.setState({ date: dateFromInput});
-    this.getPhoto(dateFromInput);
-  };
-
-
-  render () {
+  render() {
     return (
       <div>
-        <h1>NASA'S Astronomy Picture of the Day</h1>
-        <DateInput 
-        changeDate={this.changeDate}
-        />
-        <Photo 
-        photo={this.state.photo}/>
-      </div>
+        <h1>NASA's Astronomy Picture of the Day</h1>
+        <DateInput />
+        <Photo />
+        </div>
     )
   }
 }
--- src/components/DateInput.js
+++ src/components/DateInput.js
@@ -1,14 +1,6 @@
 import React from 'react';
-import DatePicker from 'react-datepicker';
-import "react-datepicker/dist/react-datepicker.css";
 
-const DateInput = props => (
-   <form onSubmit={props.changeDage}>
-       Enter a Date (YYYY-MM-DDD):
-       <input />
-       <input type="submit" />
-   </form>
-);
-
+const DateInput = props => 
+<div>DateInput</div>;
 
 export default DateInput;
